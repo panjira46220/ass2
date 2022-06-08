afacf30e36c20d1cf0ef655ff80fe31293fb2e29
panjira <6035512072@psu.ac.th>
2022-06-08 21:01:59 +0700

    3

--- pages/chart.tsx
+++ pages/chart.tsx
@@ -20,22 +20,24 @@ export default function ChartMedium():ReactElement {
     const newUrl2 =`https://api1.binance.com/api/v3/klines?interval=1h&symbol=${token1}${token2}` 
     const res2 = await  fetch(newUrl2);
     const link1 = await res2.json();
-    //console.log(link1);
+    console.log(link1);
     
 
     
     
     link1.map((item : any)=>{
-      const formatdate = dayjs(item[0]).format("YYYY-MM-DD");
+      
+      //const formatdate = dayjs(item[0]).format("YYYY-MM-DD");
       // let d = '0' + openTime.getDate().toString().slice(-2) ;
       // let m ='0' + (openTime.getMonth() + 1).toString().slice(-2);
       // let y = openTime.getFullYear();
-      
-     
+      //console.log(formatdate);
+
       const newobject ={
-        time : formatdate,
+         
+        time : item[0],
         open : Number(item[1]),
-        hight : Number( item[2]),
+        hight : Number(item[2]),
         low : Number(item[3]),
         close : Number(item[4])
       };
@@ -43,9 +45,20 @@ export default function ChartMedium():ReactElement {
       dataGraph.push(newobject);
     });
     
-    console.log(dataGraph);
-    //callChart();
     
+    console.log(dataGraph);
+     const chart = createChart(document.body, { width: 800, height: 700 });
+    // const candlestickSeries = chart.addCandlestickSeries();
+    // console.log(dataGraph);
+    // candlestickSeries.setData(dataGraph);
+    // chart.timeScale().fitContent();
+    const candlestickSeries = chart.addCandlestickSeries();
+    candlestickSeries.setData(
+      dataGraph 
+    );
+    
+    chart.timeScale().fitContent();
+
     
   
     }
@@ -57,10 +70,10 @@ export default function ChartMedium():ReactElement {
     //  chart.timeScale().fitContent();
      
    
-    const chart = createChart(document.body, { width: 800, height: 700 });
-    const candlestickSeries = chart.addCandlestickSeries({ upColor: '#26a69a', downColor: '#ef5350', borderVisible: false, wickUpColor: '#26a69a', wickDownColor: '#ef5350' });
-    candlestickSeries.setData(dataGraph);
-    chart.timeScale().fitContent();
+    // const chart = createChart(document.body, { width: 800, height: 700 });
+    // const candlestickSeries = chart.addCandlestickSeries({ upColor: '#26a69a', downColor: '#ef5350', borderVisible: false, wickUpColor: '#26a69a', wickDownColor: '#ef5350' });
+    // candlestickSeries.setData(dataGraph);
+    // chart.timeScale().fitContent();
     }  
 
     return (
--- pages/trade.tsx
+++ pages/trade.tsx
@@ -5,22 +5,33 @@ import Link from 'next/link';
 import Layout from '../components/layout';
 import styles from "../styles/Market.module.css"
 import { useState,useEffect, ReactElement } from "react";
-import { Console } from 'console';
+import  dayjs from "dayjs";
 
-export default function Marker():ReactElement{
+export default function Trade_tree():ReactElement{
   const [token1, setToken1] = useState('');
   const [token2, setToken2] = useState('');
   
-  const [allData,setAllData] =useState([{token1:"BTC",token2:"USDT",priceFTX:0,priceBinance:0,diff:0}])
-  const [display,setDisplay] = useState(0);
+  
   const [asks,setAsks] = useState([]);
   const [bids,setBids] = useState([]);
 
+  const [amount,setAmount] = useState("");
+  const [value, setValue] = useState("USDT");
+
+  const [orderDetail, setOrederdetail] = useState([]);
+  const [orderIndex, setOrederindex] = useState(0);
+
+  const [display,setDisplay] = useState(0);
+
+  let token:number = 0;
+  let priceAvg:number = 0;
+  let inputUSDT: number = Number(amount);
+  let i :number = 0;
 
   useEffect(() => {
     
   },);
- 
+ //setOrederdetail([]);
   const getApi = async (e:React.FormEvent) => { 
     e.preventDefault()
     const newUrl2 =`https://api1.binance.com/api/v3/depth?symbol=${token1}${token2}` 
@@ -28,37 +39,207 @@ export default function Marker():ReactElement{
     const link1 = await res2.json();
     setAsks(link1.asks) ;
     setBids(link1.bids);
-    someAsk()
+    
+    //someAsk()
   }
-
-  const someAsk = () => {
-    console.log(asks)
-    const someAsks = asks.filter((item:string,index:number) =>{
-      if(index<4){
-        return item;
+  //console.log(value,amount);
+  const buyValue =() => {
+   console.log(value,amount);
+   if(value == token2){  //USDT
+     for( let item of asks){
+      if(inputUSDT > 0){
+        buyOutputToken2(item);
       }
-    })
-    setAsks(someAsks)  
-    console.log(someAsks);
-      
+      else{          
+        break;
+      }
+   } 
+  }
+  else{                           //BTC
+    for( let item of asks){
+      if(inputUSDT > 0){
+        buyOutputToken1(item);
+      }
+      else{
+        break;
+      }
+    }
+  }
+  let formatdate = dayjs().format("DD/MM/YYYY h:mm");
+    const detail ={
+        time : formatdate,
+        symbol: token1+"_"+token2,
+        type :"Buy",
+        price : priceAvg/i,
+        input : amount,
+        output: token
+    }
+    orderDetail.push(detail);
+    
+    console.log(orderDetail);
+    setDisplay(display+1);
     
   }
+
+
+  function buyOutputToken2(order : any) {
+    
+    let a : number = parseFloat(order[0]);
+    let b : number = parseFloat(order[1]);
+   
+    //console.log("price= "+a+"\tAmount= "+b);
+    if(inputUSDT >= (a*b)){
+        token = b + token;
+        priceAvg = a + priceAvg;
+        inputUSDT = inputUSDT-(a*b) ;
+        i++
+        //console.log(token,priceAvg,inputUSDT);
+    }
+    else if (inputUSDT<(a*b)){  //เงินเหลือ
+        let fewToken : number = (inputUSDT*b)/a; //เหรียญที่ซื้อได้ด้วยเงินที่เหลือจริงๆ
+                            //เหรียญมีจำนวนเยอะกว่าที่ต้องการ
+            token =  fewToken + token ;
+            priceAvg = a + priceAvg; 
+            inputUSDT = 0; 
+            i++
+            console.log(token,priceAvg,inputUSDT,i);
+            
+    }
+  }
+
+  function buyOutputToken1(order : any) {
+    let a : number = parseFloat(order[0]); //ถ้าเป็นBTC
+    let b : number = parseFloat(order[1]);
+    if(inputUSDT >= b){
+      inputUSDT = inputUSDT - b;
+      token = (a*b) + token ; //ได้กี่USDT
+      priceAvg = a + priceAvg;
+      i++;
+    }
+    else if ( inputUSDT< b){
+      let fewToken :number = inputUSDT*a;
+      token = fewToken + token;
+      priceAvg = a + priceAvg;  
+      inputUSDT = 0;
+      i++
+    }
+           
+    
+  }
+
+
+  const sellValue =() => {
+    console.log(value,amount);
+   if(value == token2){  //USDT
+     for( let item of bids){
+      if(inputUSDT > 0){
+        sellOutputToken2(item);
+      }
+      else{          
+        break;
+      }
+   } 
+  }
+  else{                           //BTC
+    for( let item of bids){
+      if(inputUSDT > 0){
+        sellOutputToken1(item);
+      }
+      else{
+        break;
+      }
+    }
+  }
+  let formatdate = dayjs().format("DD/MM/YYYY h:mm");
+    const detail ={
+        time : formatdate,
+        symbol: token1+"_"+token2,
+        type :"Sell",
+        price : priceAvg/i,
+        input : amount,
+        output: token
+    }
+    orderDetail.push(detail);
+    
+    console.log(orderDetail);
+    setDisplay(display+1);
+    
+  }
+
+  function sellOutputToken2(order : any) {
+    
+    let a : number = parseFloat(order[0]);
+    let b : number = parseFloat(order[1]);
+   
+    //console.log("price= "+a+"\tAmount= "+b);
+    if(inputUSDT >= (a*b)){
+        token = b + token;
+        priceAvg = a + priceAvg;
+        inputUSDT = inputUSDT-(a*b) ;
+        i++
+        //console.log(token,priceAvg,inputUSDT);
+    }
+    else if (inputUSDT<(a*b)){  //เงินเหลือ
+        let fewToken : number = (inputUSDT*b)/a; //เหรียญที่ซื้อได้ด้วยเงินที่เหลือจริงๆ
+                            //เหรียญมีจำนวนเยอะกว่าที่ต้องการ
+            token =  fewToken + token ;
+            priceAvg = a + priceAvg; 
+            inputUSDT = 0; 
+            i++
+            console.log(token,priceAvg,inputUSDT,i);
+            
+    }
+            
+    
+  }
+  
+
+  function sellOutputToken1(order : any){
+    let a : number = parseFloat(order[0]); //ถ้าเป็นBTC
+    let b : number = parseFloat(order[1]);
+    if(inputUSDT >= b){
+      inputUSDT = inputUSDT - b;
+      token = (a*b) + token ; //ได้กี่USDT
+      priceAvg = a + priceAvg;
+      i++;
+    }
+    else if ( inputUSDT< b){
+      let fewToken :number = inputUSDT*a;
+      token = fewToken + token;
+      priceAvg = a + priceAvg;  
+      inputUSDT = 0;
+      i++
+    }
+  }
+  // const someAsk = () => {
+  //   console.log(asks)
+  //   const someAsks = asks.filter((item:string,index:number) =>{
+  //     if(index<4){
+  //       return item;
+  //     }
+  //   })
+  //   setAsks(someAsks)  
+  //   console.log(someAsks);
+      
+    
+  // }
 
   function ShowAsks (){
     return (
     <div>
+        
           <h2>Asks</h2>
           <table className={styles.wow} >
             <tr>
-              <th>Price({token1})</th>
-              <th>Amount({token2})</th>
+              <th>Price({token2})</th>
+              <th>Amount({token1})</th>
               <th>Total</th>
             </tr> 
             {asks.map((contest, idx) => (
             <tr key={idx}>
-            <td>{Number(contest[0]).toFixed(2)}</td>
-            <td>{Number(contest[1]).toFixed(2)}</td>
-            <td>{((contest[0]*contest[1])/1000).toFixed(2)}</td>
+            <td>{Number(contest[0])}</td>
+            <td>{Number(contest[1])}</td>
+            <td>{(Number(contest[0])*Number(contest[1]))}</td>
            </tr>
             ))}  
           </table>
@@ -71,8 +252,8 @@ export default function Marker():ReactElement{
           <h2>Bids</h2>
           <table className={styles.wow} >
             <tr>
-              <th>Price(${token1})</th>
-              <th>Amount(${token2})</th>
+              <th>Price({token2})</th>
+              <th>Amount({token1})</th>
               <th>Total</th>
             </tr> 
             {bids.map((contest, idx) => (
@@ -86,6 +267,38 @@ export default function Marker():ReactElement{
           </table>
     </div>
   )}
+
+  const ShowOrder =() =>{
+    return(
+      
+        <div>
+          <h2>Order history</h2>
+          <table className={styles.wow} >
+              <tr>
+                <th>Order ID</th>
+                <th>Date</th>
+                <th>Symbol</th>
+                <th>Type</th>
+                <th>Price</th>
+                <th>Input</th>
+                <th>Output</th>
+              </tr> 
+              {orderDetail.map((contest, idx) => (
+              <tr key={idx}>
+              <td>#{idx+1}</td>
+              <td>{contest.time}</td>
+              <td>{contest.symbol}</td>
+              <td>{contest.type}</td>
+              <td>{contest.price}</td>
+              <td>{contest.input}</td>
+              <td>{contest.output}</td>
+             </tr>
+              ))}  
+            </table>
+        </div>
+      
+    )
+  }
   //console.log(data);
   //console.log(token1,token2);
 
@@ -101,11 +314,19 @@ export default function Marker():ReactElement{
           <input  type="text" id="token2" name="token2" onChange={e => setToken2(e.target.value)}></input><br/>
           <input type="submit" value="Fetch" onClick={getApi}></input>
         </form>
+        {ShowAsks()}
+        {ShowBids()}
         <div>
-          {ShowAsks()}
-          {ShowBids()}
+          <h4>Amount</h4>
+          <p><input  type="text" id="amount" name="amount" onChange={(e) => setAmount(e.target.value)}></input><br/>
+          <select value={value} onChange={(e) => {setValue(e.target.value);}}>
+            <option value={token1}>{token1}</option>
+            <option value={token2}>{token2}</option>
+          </select></p>
+          <button onClick={buyValue}>Buy</button>
+          <button onClick={sellValue}>Sell</button>
           </div>
-
+          {display > 0 && <ShowOrder/> }
         </Layout>
     </div>
   )
